c11b3b6c76861eb8f6fa82f3e86af8abaef15cc7
Clay Hilgert <claytonhilgertmst@gmail.com>
2024-11-27 19:25:07 -0600

    implement mergesort

--- pkg/algorithms/mergeSort.go
+++ pkg/algorithms/mergeSort.go
@@ -1,5 +1,33 @@
 package algorithms
 
 func MergeSort(arr []string) []string {
-	return make([]string, 0)
+	if len(arr) <= 1 {
+		return arr
+	}
+	mid := len(arr) / 2
+	left := arr[:mid]
+	right := arr[mid:]
+
+	sortedLeft := MergeSort(left)
+	sortedRight := MergeSort(right)
+
+	return merge(sortedLeft, sortedRight)
+}
+
+func merge(left, right []string) []string {
+	sorted := []string{}
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			sorted = append(sorted, left[i])
+			i++
+		} else {
+			sorted = append(sorted, right[j])
+			j++
+		}
+	}
+	sorted = append(sorted, left[i:]...)
+	sorted = append(sorted, right[j:]...)
+	return sorted
 }
