d0e35c4896dd3e8b049ee690c21239897d6e07f3
Clay Hilgert <claytonhilgertmst@gmail.com>
2024-11-28 21:50:25 -0600

    implement radixsort

--- pkg/algorithms/radixSort.go
+++ pkg/algorithms/radixSort.go
@@ -1,5 +1,41 @@
 package algorithms
 
 func RadixSort(arr []string) []string {
-	return make([]string, 0)
+	maxLen := 0
+	for _, str := range arr {
+		if len(str) > maxLen {
+			maxLen = len(str)
+		}
+	}
+
+	for pos := maxLen - 1; pos >= 0; pos-- {
+		arr = sortByChar(arr, pos)
+	}
+	return arr
+}
+
+func sortByChar(arr []string, pos int) []string {
+	count := make([]int, 256)
+	output := make([]string, len(arr))
+
+	for _, str := range arr {
+		char := byte(0)
+		if pos < len(str) {
+			char = str[pos]
+		}
+		count[char]++
+	}
+	for i := 1; i < len(count); i++ {
+		count[i] += count[i-1]
+	}
+	for i := len(arr) - 1; i >= 0; i-- {
+		str := arr[i]
+		char := byte(0)
+		if pos < len(str) {
+			char = str[pos]
+		}
+		output[count[char]-1] = str
+		count[char]--
+	}
+	return output
 }
